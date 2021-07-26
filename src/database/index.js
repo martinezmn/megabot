d38cb4bf2700537192e2aae50931c8ef5ee7cdc3
Gabriel Martinez <gabrielmn378@gmail.com>
2021-07-26 14:06:40 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,11 +2,13 @@ const Sequelize = require('sequelize');
 const dbConfig = require('./config');
 
 const Member = require('../models/Member');
+const Guild = require('../models/Guild');
 const MemberHours = require('../models/MemberHours');
 
 const connection = new Sequelize(dbConfig);
 
 Member.init(connection);
+Guild.init(connection);
 MemberHours.init(connection);
 
 module.exports = connection;
