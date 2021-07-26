6883fa306c16031b33b7699c2a4a013396667d50
Gabriel Martinez <gabrielmn378@gmail.com>
2021-07-26 19:25:28 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ client.on('ready', () => {
         status: 'online',
         activity: {
             name: '!help',
-            type: "Listening"
+            type: "LISTENING"
         }
     });
                          
